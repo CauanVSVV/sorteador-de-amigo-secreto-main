b5606287192b80d0efab26fb24d404a042a03a0b
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-07-17 01:46:08 -0300

    feat: implement form component with participant name input and validation tests

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Formulario from './Formulario';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
+test("Quando o input está vazio, novos participantes não podem ser adicionados", () => {
+  render(<Formulario />);
 
-test('Quando o input está vazio, novos participantes não podem ser adicionados', () => {
-    render(<Formulario />)
+  // Encontrar no DOM o input
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  // Encontrar no DOM o botão
+  const botao = screen.getByRole("button");
 
-    // Encontrar no DOM o input
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    // Encontrar no DOM o botão
-    const botao = screen.getByRole('button')
+  // Verificar se o input está no DOM
+  expect(input).toBeInTheDocument();
+  // Verificar se o botão está desabilitado
+  expect(botao).toBeDisabled();
+});
 
-    // Verificar se o input está no DOM
-    expect(input).toBeInTheDocument()
-    // Verificar se o botão está desabilitado
-    expect(botao).toBeDisabled()
-})
+test("Adicionar um participante caso exista um nome preenchido", () => {
+  render(
+    <RecoilRoot>
+      <Formulario />
+    </RecoilRoot>
+  );
+
+  // Encontrar no DOM o input
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  // Encontrar no DOM o botão
+  const botao = screen.getByRole("button");
+
+  fireEvent.change(input, { target: { value: "Ana Catarina" } });
+  fireEvent.click(botao);
+
+  expect(input).toHaveFocus();
+  expect(botao).toHaveValue("");
+});
--- src/componentes/Formulario.tsx
+++ src/componentes/Formulario.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useAdicionarParticipante } from "./state/hook/useAdicionarParticipante";
 
 const Formulario = () => {
-  return (
-    <form>
-        <input type="text" placeholder="Insira os nomes dos participantes" />
-        <button disabled={true}>Adicionar participante</button>
-    </form>
-  )
-}
+  const [nome, setNome] = useState("");
 
-export default Formulario
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const adicionarNaLista = useAdicionarParticipante()
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    adicionarNaLista(nome)
+    setNome("");
+    inputRef.current?.focus();
+  };
+
+  return (
+    <form onSubmit={adicionarParticipante}>
+      <input ref={inputRef} value={nome} onChange={evento => setNome(evento.target.value)} type="text" placeholder="Insira os nomes dos participantes" />
+      <button disabled={!nome}>Adicionar</button>
+    </form>
+  );
+};
+
+export default Formulario;
